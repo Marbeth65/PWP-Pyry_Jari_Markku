a7ebd24335207c443ffcbec51cef7c7b98f49d46
Jari <jari.jussila@student.oulu.fi>
2020-02-12 11:22:04 +0200

    calculators codes
    calculators JavaScript, HTML and CSS codes

--- JavaScript/carcalculator.js
+++ JavaScript/carcalculator.js
@@ -1,37 +1,37 @@
 //Car price calculator. On muokattu, ei ole enää niin alkuperäinen. Muokkauspäivä 23.1
 function CarCalculate() {
-  var carPrice = document.getElementById("feed car price").value;
-  var serviceQual = document.getElementById("somethingsomething").value;//keksittävä käyttötarkoitus
-  var partofmonth = document.getElementById("part of month").value;//kuukausierä
+  var carPrice = document.getElementById("car_price").value;
+  var handmoney = document.getElementById("hand_money").value;//keksittävä käyttötarkoitus
+  var partofmonth = document.getElementById("monthly_fee").value;//kuukausierä
 
   //validate input
-  if (carPrice === "" || serviceQual == 0) {
+  if (carPrice === "" || handmoney == 0) {
     alert("Please enter values");
     return;
   }
   //Check to see if this input is empty or less than or equal to 1
   if (partofmonth === "" || partofmonth <= 1) {
     partofmonth = 1;
-    document.getElementById("each").style.display = "none";
+    document.getElementById("Sold_of_car").style.display = "none";
   } else {
-    document.getElementById("each").style.display = "block";
+    document.getElementById("Sold_of_car").style.display = "block";
   }
 
-  //Calculate tip
-  var total = (carPrice * serviceQual) / partofmonth;
+  //Calculate car price
+  var total = ((carPrice - handmoney)//tähän tulee sitten muut laskutoimitukset) / partofmonth;
   //round to two decimal places
   total = Math.round(total * 100) / 100;
   //next line allows us to always have two digits after decimal point
   total = total.toFixed(2);
   //Display the tip
   document.getElementById("totalprice").style.display = "block";
-  document.getElementById("tip").innerHTML = total;
+  document.getElementById("tipcar").innerHTML = total;//html rivi 36
 
 }
 
 //Hide the tip amount on load
-document.getElementById("totalprice").style.display = "none";
-document.getElementById("each").style.display = "none";
+document.getElementById("tipcar").style.display = "none";//html rivi 35
+document.getElementById("Sold_of_car").style.display = "none";
 
 //click to call function
 document.getElementById("calculate").onclick = function() {
