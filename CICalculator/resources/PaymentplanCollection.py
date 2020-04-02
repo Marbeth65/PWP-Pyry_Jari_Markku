fc214e9b62504f675ba16498aad836ce61fffb30
Marbeth65 <mhannula61@gmail.com>
2020-04-02 20:03:59 +0300

    Update PaymentplanCollection.py
    Ei testattu kaikkia, keskeneräinen

--- CICalculator/resources/PaymentplanCollection.py
+++ CICalculator/resources/PaymentplanCollection.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource
-from CICalculator.models import Handle, Paymentplan
+from CICalculator.models import Handle
 
 '''
 Serves as a collection of all paymentplans
@@ -19,13 +19,14 @@ class PaymentplanCollection(Resource):
             plans = x.paymentplans
             planlist = []
             for y in plans:
-                planlist.append((y.price,
-                                 y.provider,
-                                 y.interestrate, 
-                                 y.months, 
-                                 y.payers, 
-                                 y.open, 
-                                 y.owner_name))
+                planlist.append({
+                    "price":y.price,
+                    "provider":y.provider,
+                    "interestrate":y.interestrate, 
+                    "months":y.months, 
+                    "payers":y.payers, 
+                    "open":y.open, 
+                    "owner_name":y.owner_name})
             list.append({
                 "handle":x.handle,
                 "name":x.name,
@@ -34,31 +35,57 @@ class PaymentplanCollection(Resource):
             })
             
         return list, 201
-         
-        
+           
     
     def put(self):
         '''
         modify existing paymentplan by replacing the values with new ones
         '''
-        list = []
-        list.append("toimii")
-        return list, 201
+        handle = request.json["handle"]
+        name = request.json["name"]
+        type = request.json["type"]
+        
+        Handle.query.filter_by(handle=handle).update({
+            "name": name,
+            "type": type   
+        })
+        db.session.commit()
+        return "", 201
+        
         
     def post(self):
         '''
         post a new paymentplan
         '''
-        list = []
-        list.append("toimii")
-        return list, 201
+        
+        if not request.json:
+            abort(415)
+            
+        try:
+            item = Handle(
+                handle=request.json["handle"],
+                name=request.json["name"],
+                type=request.json["type"],
+            )
+            db.session.add(item)
+            db.session.commit()
+        except KeyError:
+            abort(400)
+        except IntegrityError:
+            abort(409)
+        
+        return "", 201
+
         
     def delete(self):
         '''
         deletes a handle and everything related to that handle
         '''
-        list = []
-        list.append("toimii")
-        return list, 201
+        handle = request.json["handle"]
+        item = Handle.query.filter_by(handle=handle).first()
+        db.session.delete(item)
+        db.session.commit()
+
+        return "", 201
 
 print("PaymentplanCollection working")
