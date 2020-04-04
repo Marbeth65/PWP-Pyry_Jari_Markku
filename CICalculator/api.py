6ab26ccdae7f8d4b4b7a8e394ac6751070b816d1
Pyry Saarinen <saarinenps@gmail.com>
2020-04-04 17:10:04 +0300

    upgraded

--- CICalculator/api.py
+++ CICalculator/api.py
@@ -5,8 +5,13 @@ api_bp = Blueprint("api", __name__)
 api = Api(api_bp)
 
 from CICalculator.resources.PaymentplanCollection import PaymentplanCollection
+from CICalculator.resources.OpenPaymentplanCollection import OpenPaymentplanCollection
+# from CICalculator.resources.ModelCollection import ModelCollection // modeli ei vielä valmis
+
 
 api.add_resource(PaymentplanCollection, "/api/plans")
+api.add_resource(OpenPaymentplanCollection, "/api/plans/open")
+#api.add_resource(OpenPaymentplanCollection, "/api/models") tämä on sitten, kun saan noi modelit toimimaan.
 
 @api_bp.route("/")
 def index():
