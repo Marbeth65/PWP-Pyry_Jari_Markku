756293ffb929e5d3b3e09e1d9e715d439beda50e
Pyry Saarinen <saarinenps@gmail.com>
2020-05-13 12:32:18 +0300

    hypermedia lisätty

---
+++ CICalculator/utils/hypermedia.py
@@ -0,0 +1,98 @@
+from flask import url_for
+
+''' Hypermedia controls to "dummyhandle" '''
+
+class MasonBuilder(dict):
+
+    def add_control(self, control_name, href, **kwargs):
+    
+        if "@controls" not in self:
+            self["@controls"] = {}
+            
+        self["@controls"][control_name] = kwargs
+        self["@controls"][control_name]["href"] = href
+        
+    
+class PaymentCollectionBuilder(MasonBuilder):
+    pass
+
+        
+class CICalcBuilder(MasonBuilder):
+    
+    @staticmethod
+    def generate_model_schema():
+        schema = {
+        "type": "object",
+        "required": ["manufacturer", "model", "year"]
+        }
+        
+        properties = schema["properties"] = {}
+        
+        properties["manufacturer"] = {
+            "type": "string",
+            "description": "manufacturer of model"
+        }
+        
+        properties["model"] = {
+            "type": "string"
+        }
+        
+        properties["year"] = {
+            "type": "number"
+        }
+        return schema
+        
+    @staticmethod
+    def generate_payment_post_schema():
+        schema = {
+        "type": "object",
+        "required": ["provider", "price", "months"]
+        }
+        
+        properties = schema["properties"] = {}
+        
+        properties["provider"] = {
+        "type": "string"
+        }
+        properties["price"] = {
+        "type": "number"
+        }
+        
+        properties["months"] = {
+        "type": "number"
+        }
+        
+        return schema
+    
+    def add_control_toggle(self, url):
+    
+        self.add_control("cicalc:toggle", url, method="PUT", description="Toggles plans open-field")
+        
+    def add_control_paymentplans_all(self):
+        self.add_control("cicalc:plans-all", "/api/dummyhandle/plans", method="GET")
+        
+    def add_control_paymentplan_item(self, href):
+    
+        self.add_control("self", href, method="GET")
+        
+    def add_control_models_all(self):
+    
+        self.add_control("cicalc:models-all", "/api/dummyhandle/models", method="GET")
+        
+    def add_control_paymentplans_open(self):
+        
+        self.add_control("cicalc:plans-open", "/api/dummyhandle/plans/open", method="GET")
+
+    def add_control_post_model(self):
+        
+        self.add_control("cicalc:add-model", "/api/dummyhandle/models", method="POST", schema=self.generate_model_schema())
+        
+    def add_control_append_plan(self, url):
+        
+        self.add_control("cicalc:append-plan", url, method="POST", schema=self.generate_payment_post_schema())
+        
+    def add_control_asso(self, url):
+    
+        self. add_control("cicalc:asso", url, method="GET")
+
+    
